c9181343ecf16bdebe190be53c79fe7a81b823a5
akowalew <ram.techen@gmail.com>
2016-03-02 19:12:57 +0100

    first commit

---
+++ Debug/main.pp
@@ -0,0 +1,47 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+main.obj: /opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_types.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_memmap.h
+main.obj: /home/dicker/Embedded/tivaware/inc/tm4c123gh6pm.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_gpio.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_timer.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_uart.h
+main.obj: /home/dicker/Embedded/tivaware/inc/hw_adc.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/sysctl.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/gpio.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/interrupt.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/timer.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/pin_map.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/uart.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/pwm.h
+main.obj: /home/dicker/Embedded/tivaware/driverlib/adc.h
+
+../main.c: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+/home/dicker/Embedded/tivaware/inc/hw_types.h: 
+/home/dicker/Embedded/tivaware/inc/hw_memmap.h: 
+/home/dicker/Embedded/tivaware/inc/tm4c123gh6pm.h: 
+/home/dicker/Embedded/tivaware/inc/hw_gpio.h: 
+/home/dicker/Embedded/tivaware/inc/hw_timer.h: 
+/home/dicker/Embedded/tivaware/inc/hw_uart.h: 
+/home/dicker/Embedded/tivaware/inc/hw_adc.h: 
+/home/dicker/Embedded/tivaware/driverlib/sysctl.h: 
+/home/dicker/Embedded/tivaware/driverlib/gpio.h: 
+/home/dicker/Embedded/tivaware/driverlib/interrupt.h: 
+/home/dicker/Embedded/tivaware/driverlib/timer.h: 
+/home/dicker/Embedded/tivaware/driverlib/pin_map.h: 
+/home/dicker/Embedded/tivaware/driverlib/uart.h: 
+/home/dicker/Embedded/tivaware/driverlib/pwm.h: 
+/home/dicker/Embedded/tivaware/driverlib/adc.h: 
---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_timer.h"
+#include "inc/hw_uart.h"
+#include "inc/hw_adc.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/uart.h"
+#include "driverlib/pwm.h"
+#include "driverlib/adc.h"
+
+int main(void) {
+	
+	return 0;
+}
